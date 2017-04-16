7853fcebaa043c5205ca5f48e383908436fd8379
Romain Porte <microjoe@microjoe.org>
2017-04-16 15:03:39 +0200

    Bump to version 1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -7,6 +7,9 @@ This project adheres to `Semantic Versioning <http://semver.org/>`__.
 [Unreleased]
 ------------
 
+[1.1] - 2017-04-16
+------------------
+
 - Added support for external toots
 
 [1.0] - 2017-04-09
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hms_mastodon',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     scripts=['bin/hms_mastodon'],
 
